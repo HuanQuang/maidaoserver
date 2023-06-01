ee7336181f52669eab164f998aecb14b60e0136d
Trần Quang Huấn <hyugalaputa@gmail.com>
2023-06-01 23:55:08 +0700

    first commit

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ import order from './Router/router.js'
 dotenv.config()
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-const port = process.env['PORT'] || 8000
-app.use(cors())
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false,  limit: '50mb'}))
+app.use(bodyParser.json({ limit: '50mb' }))
 const mongo_username = process.env['MONGO_USERNAME']
 const mongo_password = process.env['MONGO_PASSWORD']
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.snqpcmm.mongodb.net/?retryWrites=true&w=majority`
+const port = process.env['PORT'] || 8000
+app.use(cors())
+app.use(express.json())
 app.use('/api/order',order)
 mongoose.connect(uri, {
     useUnifiedTopology: true,
